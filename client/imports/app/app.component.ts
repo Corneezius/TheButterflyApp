82400d49199d885547a67a379120965d20810a3d
Corneezius <cfr2116@columbia.edu>
2017-02-15 22:51:40 -0500

    add ideas page to app componen and set it as root

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 // import ionic components
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
+import { IdeasPage } from '../pages/ideas/ideas';
 import template from "./app.html";
 
 @Component({
@@ -9,6 +10,8 @@ import template from "./app.html";
   template
 })
 export class MyApp {
+    rootPage = IdeasPage;
+
   constructor(platform: Platform) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -1,17 +1,21 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { IdeasPage } from '../pages/ideas/ideas'
+
 import { MyApp } from './app.component';
 
 @NgModule({
   declarations: [
-    MyApp
+    MyApp,
+    IdeasPage
   ],
   imports: [
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp
+    MyApp,
+    IdeasPage
   ],
 
   providers: [
