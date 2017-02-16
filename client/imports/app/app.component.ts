79363e85cd4e835d949596531ba92e65afd9b8c3
Corneezius <cfr2116@columbia.edu>
2017-02-15 20:13:23 -0500

    import ionic components and modules

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+// import ionic components
+import { Platform } from 'ionic-angular';
+import { StatusBar, Splashscreen } from 'ionic-native';
 import template from "./app.html";
 
 @Component({
   selector: 'my-app',
   template
 })
-export class MyApp {}
+export class MyApp {
+  constructor(platform: Platform) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Check to see if its running on mobile
+      if (platform.is('cordova')) {
+        StatusBar.styleDefault();
+        Splashscreen.hide();
+      }
+    });
+  }
+}
