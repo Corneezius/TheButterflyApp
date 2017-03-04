32cee51ec1cd00175942d23c4f272ffacb17ef29
Corneezius <cfr2116@columbia.edu>
2017-03-04 18:17:55 -0500

    add showmessages

--- client/imports/pages/ideas/ideas.ts
+++ client/imports/pages/ideas/ideas.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from 'ionic-angular';
 import * as Moment from 'moment';
 import { Observable } from 'rxjs';
 import { Ideas, Messages } from '../../../../imports/collections';
@@ -11,26 +12,30 @@ import template from './ideas.html';
 export class IdeasPage implements OnInit {
   ideas;
 
-  constructor() {
+  constructor(private navCtrl: NavController) {
   }
 
   ngOnInit() {
-    this.ideas = Ideas
-      .find({})
-      .mergeMap((ideas: Idea[]) =>
-        Observable.combineLatest(
-          ...ideas.map((idea: Idea) =>
-            Messages
-              .find({ideaId: idea._id})
-              .startWith(null)
-              .map(messages => {
-                if (messages) idea.lastMessage = messages[0];
-                return idea;
-              })
-          )
+  this.ideas = Ideas
+    .find({})
+    .mergeMap((ideas: Idea[]) =>
+      Observable.combineLatest(
+        ...ideas.map((idea: Idea) =>
+          Messages
+            .find({ideaId: idea._id})
+            .startWith(null)
+            .map(messages => {
+              if (messages) idea.lastMessage = messages[0];
+              return idea;
+            })
         )
-      ).zone();
-  }
+      )
+    ).zone();
+}
+
+showMessages(idea): void {
+  this.navCtrl.push(MessagesPage, {idea});
+}
 
   removeIdea(idea: Idea): void {
     Ideas.remove({_id: idea._id}).subscribe(() => {});
