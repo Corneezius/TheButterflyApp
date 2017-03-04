3e35beaf634142ff3bb5e35d8184e18882c2692f
Corneezius <cfr2116@columbia.edu>
2017-03-04 18:33:09 -0500

    Add basic messages component

---
+++ client/imports/pages/messages.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { NavParams } from 'ionic-angular';
+import { Idea, Message } from '../../../../imports/models';
+import template from './messages.html';
+
+@Component({
+  template
+})
+export class MessagesPage implements OnInit {
+  selectedIdea: Idea;
+
+  constructor(navParams: NavParams) {
+    this.selectedIdea = <Idea>navParams.get('idea');
+    this.title = this.selectedIdea.title;
+    this.picture = this.selectedIdea.picture;
+
+    console.log('Selected idea is: ', this.selectedIdea);
+  }
+
+  ngOnInit() {
+    let isEven = false;
+
+    this.messages = Messages.find(
+      {ideaId: this.selectedIdea._id},
+      {sort: {createdAt: 1}}
+    ).map((messages: Message[]) => {
+      messages.forEach((message: Message) => {
+        message.ownership = isEven ? 'mine' : 'other';
+        isEven = !isEven;
+      });
+
+      return messages;
+    });
+  }
+}
