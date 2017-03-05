c880ae5a2948b0f114c534beb361ab139ca0fd31
Corneezius <cfr2116@columbia.edu>
2017-03-04 22:33:16 -0500

    wait for user to login

--- client/main.ts
+++ client/main.ts
@@ -1,10 +1,18 @@
 import 'zone.js';
 import 'reflect-metadata';
+import { MeteorObservable } from 'meteor-rxjs';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { Meteor } from 'meteor/meteor';
 import { AppModule } from './imports/app/app.module';
 
 Meteor.startup(() => {
-  platformBrowserDynamic().bootstrapModule(AppModule);
+  const subscription = MeteorObservable.autorun().subscribe(() => {
+    if (Meteor.loggingIn()) {
+      return;
+    }
+
+    setTimeout(() => subscription.unsubscribe());
+    platformBrowserDynamic().bootstrapModule(AppModule);
+  });
 });
