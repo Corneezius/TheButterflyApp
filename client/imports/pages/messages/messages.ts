1d210fdda7f5998ab57f823b0ed1e7f773f7e32f
Corneezius <cfr2116@columbia.edu>
2017-03-04 19:43:58 -0500

    add implement sendTextmessage

--- client/imports/pages/messages/messages.ts
+++ client/imports/pages/messages/messages.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NavParams } from 'ionic-angular';
+import { MeteorObservable } from 'meteor-rxjs';
+import { Observable } from 'rxjs';
 import { Messages } from '../../../../imports/collections';
-import { Idea, Message } from '../../../../imports/models';
+import { Idea, Message, MessageType } from '../../../../imports/models';
 import template from './messages.html';
 
 @Component({
@@ -9,10 +11,14 @@ import template from './messages.html';
 })
 export class MessagesPage implements OnInit {
   selectedIdea: Idea;
+  title: string;
+ picture: string;
+ messages: Observable<Message[]>;
+ message: string = '';
 
   constructor(navParams: NavParams) {
     this.selectedIdea = <Idea>navParams.get('idea');
-    
+
     this.title = this.selectedIdea.title;
     this.picture = this.selectedIdea.picture;
 
@@ -32,6 +38,27 @@ export class MessagesPage implements OnInit {
       });
 
       return messages;
+    });
+  }
+
+  onInputKeypress({ keyCode }: KeyboardEvent): void {
+    if (keyCode === 13) {
+      this.sendTextMessage();
+    }
+  }
+
+  sendTextMessage(): void {
+    // If message was yet to be typed, abort
+    if (!this.message) {
+      return;
+    }
+
+    MeteorObservable.call('addMessage', MessageType.TEXT,
+      this.selectedIdea._id,
+      this.message
+    ).zone().subscribe(() => {
+      // Zero the input field
+      this.message = '';
     });
   }
 }
