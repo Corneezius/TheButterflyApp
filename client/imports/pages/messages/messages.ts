53acc8708ef096d9de344231bb1be024b179f2c3
Corneezius <cfr2116@columbia.edu>
2017-03-04 19:53:38 -0500

    implement addmessage and mutation observer to scroll down whenever new message added

--- client/imports/pages/messages/messages.ts
+++ client/imports/pages/messages/messages.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ElementRef } from '@angular/core';
 import { NavParams } from 'ionic-angular';
 import { MeteorObservable } from 'meteor-rxjs';
 import { Observable } from 'rxjs';
@@ -9,14 +9,20 @@ import template from './messages.html';
 @Component({
   template
 })
-export class MessagesPage implements OnInit {
+export class MessagesPage implements OnInit, OnDestroy {
   selectedIdea: Idea;
   title: string;
  picture: string;
  messages: Observable<Message[]>;
  message: string = '';
+ autoScroller: MutationObserver;
+ scrollOffset = 0;
 
-  constructor(navParams: NavParams) {
+
+ constructor(
+   navParams: NavParams,
+   private el: ElementRef
+ ) {
     this.selectedIdea = <Idea>navParams.get('idea');
 
     this.title = this.selectedIdea.title;
@@ -25,7 +31,21 @@ export class MessagesPage implements OnInit {
     console.log('Selected idea is: ', this.selectedIdea);
   }
 
+  private get messagesPageContent(): Element {
+   return this.el.nativeElement.querySelector('.messages-page-content');
+ }
+
+ private get messagesList(): Element {
+   return this.messagesPageContent.querySelector('.messages');
+ }
+
+ private get scroller(): Element {
+   return this.messagesList.querySelector('.scroll-content');
+ }
+
   ngOnInit() {
+    this.autoScroller = this.autoScroll();
+
     let isEven = false;
 
     this.messages = Messages.find(
@@ -41,6 +61,28 @@ export class MessagesPage implements OnInit {
     });
   }
 
+  ngOnDestroy() {
+    this.autoScroller.disconnect();
+  }
+
+  autoScroll(): MutationObserver {
+    const autoScroller = new MutationObserver(this.scrollDown.bind(this));
+
+    autoScroller.observe(this.messagesList, {
+      childList: true,
+      subtree: true
+    });
+
+    return autoScroller;
+  }
+
+  scrollDown(): void {
+    // Scroll down and apply specified offset
+    this.scroller.scrollTop = this.scroller.scrollHeight - this.scrollOffset;
+    // Zero offset for next invocation
+    this.scrollOffset = 0;
+  }
+ 
   onInputKeypress({ keyCode }: KeyboardEvent): void {
     if (keyCode === 13) {
       this.sendTextMessage();
